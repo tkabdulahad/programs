6eefa256c663cd780424ea5dee227cc0a8d8cbb7
tkabdulahad <31615037+tkabdulahad@users.noreply.github.com>
2017-09-07 08:36:24 +0530

    Add files via upload

--- test7.py
+++ test7.py
@@ -9,39 +9,39 @@ while count<=100:
 		count=count+r
 		print("you got",r)
 		print("your current location is",count)
-		if count==8:
-		 count=37
+		if count==8:	
+			count=37
 			print("climbed lader",count)
-		elif count==13:
+		elif count==13:	
 			count=34
 			print("climbed lader",count)
-		elif count==40:
+		elif count==40:	
 			count=68
 			print("climbed lader",count)
-		elif count==52:
+		elif count==52:	
 			count=81
 			print("climbed lader",count)
-		elif count==76:
+		elif count==76:	
 			count=97
 			print("climbed lader",count)
-		elif count==11:
+		elif count==11:	
 			count=2
 			print("snake bit",count)
-		elif count==25:
+		elif count==25:	
 			count=4
 			print("snake bit",count)
-		elif count==38:
+		elif count==38:	
 			count=9
 			print("snake bit",count)
-		elif count==65:
+		elif count==65:	
 			count=46
 			print("snake bit",count)
-	elif count==89:
+	elif count==89:	
 		count=70
 		print("snake bit",count)
-elif count==93:
-	count=64
-	print("snake bit",count)
+	elif count==93:	
+		count=64
+		print("snake bit",count)
 
 
 		
